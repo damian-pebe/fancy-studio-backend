837d1cb9d122ce1f1dc732df717d1a419768adec
damian-pebe <damian.pebe@gmail.com>
2025-06-08 15:07:51 -0600

    new railway url done

--- environment.js
+++ environment.js
@@ -1,5 +1,5 @@
-const BASE_URL = 'http://localhost:8080'
-// const BASE_URL = "BASE_URL";
+// const BASE_URL = 'http://localhost:8080'
+const BASE_URL = "https://fancy-studio-backend-production.up.railway.app";
 
 const bookMeeting =
   "https://wqjkphuykivijzjmnada.supabase.co/storage/v1/object/public/fancy-microblading//agendarCitaFancy.png";
